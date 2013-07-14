a66ef7c6810915611edae3c51ffd30d7c16d23a8
Chenzhutian <chenzhutian1992@gmail.com>
2013-07-14 15:39:15 +0800

    Test the network

--- src/com/cike/swarmbasedrobot/Expriment.java
+++ src/com/cike/swarmbasedrobot/Expriment.java
@@ -1,5 +1,13 @@
 package com.cike.swarmbasedrobot;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -19,10 +27,10 @@ public class Expriment {
 
 		int p = 0;
 		int tempSumMass;
-		e.init(20, 10,400);
+		e.init(20, 10, 400);
 		e.computSumMass();
 		tempSumMass = e.sumMass;
-
+		e.printMap();
 		while (e.sumMass > 0) {
 			int i;
 			Robot tempRobot = null;
@@ -39,9 +47,9 @@ public class Expriment {
 
 			for (i = 0; i < e.allRobots.size(); ++i) {
 				tempRobot = e.allRobots.get(i);
-				 tempRobot.pickDestinationByPSO();
+				tempRobot.pickDestinationByPSO();
 				// tempRobot.pickDestinationByRandom();
-				//tempRobot.pickDestinationByMotifyPSO();
+				// tempRobot.pickDestinationByMotifyPSO();
 			}
 
 			for (i = 0; i < e.allRobots.size(); ++i) {
@@ -68,73 +76,84 @@ public class Expriment {
 				break;
 		}
 	}
-	
-//	public static void main(String[] args) throws Exception {
-//		Expriment e = new Expriment();
-//		int sum = 0;
-//		int k = 200;
-//		int min = 33333333;
-//		while (k > 0) {
-//			e = new Expriment();
-//			e.init(20, 10,400);
-//			// int p = 0;
-//			int t = 0;
-//			// int tempSumMass;
-//			// tempSumMass = e.sumMass;
-//
-//			while (e.sumMass > 0) {
-//				// / e.printBounder();
-//				int i;
-//				Robot tempRobot = null;
-//				for (i = 0; i < e.allRobots.size(); ++i) {
-//					tempRobot = e.allRobots.get(i);
-//					tempRobot.detected(tempRobot.getX(), tempRobot.getY());
-//				}
-//
-////				for (i = 0; i < e.allRobots.size(); ++i) {
-////					tempRobot = e.allRobots.get(i);
-////					tempRobot.getNeighbour();
-////				}
-//
-//				for (i = 0; i < e.allRobots.size(); ++i) {
-//					tempRobot = e.allRobots.get(i);
-//					//tempRobot.pickDestinationByPSO();
-//					//tempRobot.pickDestinationByMotifyPSO();
-//					tempRobot.pickDestinationByRandom();
-//				}
-//
-//				for (i = 0; i < e.allRobots.size(); ++i) {
-//					tempRobot = e.allRobots.get(i);
-//					if (tempRobot.tryToRemove() != null) {
-//						int tempx = tempRobot.tryToRemove().get("x").intValue();
-//						int tempy = tempRobot.tryToRemove().get("y").intValue();
-//						tempRobot.remove(tempx, tempy);
-//					} else {
-//						tempRobot.move();
-//					}
-//				}
-//				// e.printMap();
-//				// Thread.sleep(200);
-//				e.computSumMass();
-//				// if (e.sumMass == tempSumMass) {
-//				// p++;
-//				// } else {
-//				// tempSumMass = e.sumMass;
-//				// p = 0;
-//				// }
-//				t++;
-//				// if (p > 100)break;
-//			}
-//			if (t < min)
-//				min = t;
-//			System.out.println(t);
-//			sum += t;
-//			k--;
-//		}
-//		System.out.println(sum + " and avg is " + sum / 200 + ", and min is "
-//				+ min);
-//	}
 
+	// public static void main(String[] args) throws Exception {
+	// Expriment e = new Expriment();
+	// int sum = 0;
+	// int k = 1000;
+	// int min = 33333333;
+	// int max = 0;
+	// //ArrayList<Integer> data = new ArrayList<>();
+	// FileWriter fw = new FileWriter("t.txt",true);
+	// while (k > 0) {
+	// e = new Expriment();
+	// e.init(10, 10, 400);
+	// int t = 0;
+	// // int p = 0;
+	// // int tempSumMass;
+	// // tempSumMass = e.sumMass;
+	//
+	// while (e.sumMass > 0) {
+	// // / e.printBounder();
+	// int i;
+	// Robot tempRobot = null;
+	// for (i = 0; i < e.allRobots.size(); ++i) {
+	// tempRobot = e.allRobots.get(i);
+	// tempRobot.detected(tempRobot.getX(), tempRobot.getY());
+	// }
+	//
+	// for (i = 0; i < e.allRobots.size(); ++i) {
+	// tempRobot = e.allRobots.get(i);
+	// tempRobot.getNeighbour();
+	// }
+	//
+	// for (i = 0; i < e.allRobots.size(); ++i) {
+	// tempRobot = e.allRobots.get(i);
+	// tempRobot.pickDestinationByPSO();
+	// //tempRobot.pickDestinationByMotifyPSO();
+	// //tempRobot.pickDestinationByRandom();
+	// }
+	//
+	// for (i = 0; i < e.allRobots.size(); ++i) {
+	// tempRobot = e.allRobots.get(i);
+	// if (tempRobot.tryToRemove() != null) {
+	// int tempx = tempRobot.tryToRemove().get("x").intValue();
+	// int tempy = tempRobot.tryToRemove().get("y").intValue();
+	// tempRobot.remove(tempx, tempy);
+	// } else {
+	// tempRobot.move();
+	// }
+	// }
+	// e.computSumMass();
+	// // if (e.sumMass == tempSumMass) {
+	// // p++;
+	// // } else {
+	// // tempSumMass = e.sumMass;
+	// // p = 0;
+	// // }
+	// // if (p > 100)break;
+	// t++;
+	// }
+	// if (t < min)
+	// min = t;
+	// if (t > max)
+	// max = t;
+	// //System.out.println(t);
+	// //fw.write(String.valueOf(t)+"\r\n");
+	// sum += t;
+	// k--;
+	//
+	// }
+	// //fw.flush();
+	// //fw.close();
+	// System.out.println(sum + " and avg is " + sum / 1000 + ", and min is "
+	// + min+", and max is "+max);
+	// }
+
+	// public static void main(String[] args) throws NumberFormatException,
+	// IOException{
+	// System.out.print(Expriment.computAvg("i64"));
+	// }
 	public void computSumMass() {
 		sumMass = 0;
 		Target tempTarget;
@@ -144,7 +163,30 @@ public class Expriment {
 		}
 	}
 
-	public void init(int r, int t,int mass) {
+	public static double computAvg(String fileName)
+			throws NumberFormatException, IOException {
+		File file = new File(fileName + ".txt");
+		InputStreamReader read = new InputStreamReader(
+				new FileInputStream(file));
+		BufferedReader br = new BufferedReader(read);
+		String s = null;
+		int[] count = new int[20000];
+		for (int i = 0; i < 20000; ++i) {
+			count[i] = 0;
+		}
+		while ((s = br.readLine()) != null) {
+			int temp = Integer.valueOf(s);
+			count[temp]++;
+		}
+
+		int avg = 0;
+		for (int i = 0; i < 20000; ++i) {
+			avg += i * count[i];
+		}
+		return avg * 0.001;
+	}
+
+	public void init(int r, int t, int mass) {
 		// step1
 		// 初始化地图，随机分布机器人和目标
 		Pipe.SumMass = mass;
@@ -155,11 +197,11 @@ public class Expriment {
 			int tempy = rd.nextInt(40);
 			if (map.GlobalMap[tempx][tempy] == 0) {
 				map.GlobalMap[tempx][tempy] = Map.TARGET;
-				Target tempTarget = new Target(tempx, tempy,i,10,1);
+				Target tempTarget = new Target(tempx, tempy, i, t, 0);
 				allTargets.add(tempTarget);
 				Pipe.setTargetByLocation(tempx, tempy, tempTarget);
 				++i;
-				System.out.println(tempTarget.getMass());
+				// System.out.println(tempTarget.getMass());
 			}
 		}
 
@@ -168,8 +210,8 @@ public class Expriment {
 			int tempy = rd.nextInt(40);
 			if (map.GlobalMap[tempx][tempy] == 0) {
 				map.GlobalMap[tempx][tempy] = Map.ROBOT;
-				//Robot tempRobot = new Robot(tempx, tempy, 3, 10);
-				Robot tempRobot = new Robot(tempx,tempy,3,4,10);
+				// Robot tempRobot = new Robot(tempx, tempy, 3, 10);
+				Robot tempRobot = new Robot(tempx, tempy, 4, 3, 10);
 				allRobots.add(tempRobot);
 				Pipe.setRobotByLocation(tempx, tempy, tempRobot);
 				++i;
@@ -183,7 +225,7 @@ public class Expriment {
 		}
 		Pipe.list_allRobots = this.allRobots;
 		Pipe.allTargets = this.allTargets;
-		
+
 	}
 
 	// public void printBounder() {
@@ -223,7 +265,10 @@ public class Expriment {
 					if (this.map.getGlobalMap()[i - 1][j - 1] == 0) {
 						System.out.print("_");
 					} else {
-						System.out.print(this.map.getGlobalMap()[i - 1][j - 1]);
+						if (this.map.getGlobalMap()[i - 1][j - 1] == Map.ROBOT)
+							System.out.print("*");
+						else if (this.map.getGlobalMap()[i - 1][j - 1] == Map.TARGET)
+							System.out.print("@");
 					}
 				}
 			}
